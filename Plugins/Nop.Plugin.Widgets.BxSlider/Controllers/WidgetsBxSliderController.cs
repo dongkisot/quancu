56fb6b7b5d22eac4d3a2445783198f764070dd2c
linhhdinh <linh.hoang.dinh@gmail.com>
2013-08-22 15:59:31 +0700

    style homepage & product detail
    modify mediasetting in database

---
+++ Plugins/Nop.Plugin.Widgets.BxSlider/Controllers/WidgetsBxSliderController.cs
@@ -0,0 +1,191 @@
+﻿using System.Web.Mvc;
+using Nop.Core;
+using Nop.Plugin.Widgets.BxSlider.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Media;
+using Nop.Services.Stores;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Widgets.BxSlider.Controllers
+{
+    public class WidgetsBxSliderController : Controller
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly IStoreService _storeService;
+        private readonly IPictureService _pictureService;
+        private readonly ISettingService _settingService;
+
+        public WidgetsBxSliderController(IWorkContext workContext,
+            IStoreContext storeContext, IStoreService storeService, 
+            IPictureService pictureService, ISettingService settingService)
+        {
+            this._workContext = workContext;
+            this._storeContext = storeContext;
+            this._storeService = storeService;
+            this._pictureService = pictureService;
+            this._settingService = settingService;
+        }
+        
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var bxSliderSettings = _settingService.LoadSetting<BxSliderSettings>(storeScope);
+            var model = new ConfigurationModel();
+            model.Picture1Id = bxSliderSettings.Picture1Id;
+            model.Text1 = bxSliderSettings.Text1;
+            model.Link1 = bxSliderSettings.Link1;
+            model.Picture2Id = bxSliderSettings.Picture2Id;
+            model.Text2 = bxSliderSettings.Text2;
+            model.Link2 = bxSliderSettings.Link2;
+            model.Picture3Id = bxSliderSettings.Picture3Id;
+            model.Text3 = bxSliderSettings.Text3;
+            model.Link3 = bxSliderSettings.Link3;
+            model.Picture4Id = bxSliderSettings.Picture4Id;
+            model.Text4 = bxSliderSettings.Text4;
+            model.Link4 = bxSliderSettings.Link4;
+            model.ActiveStoreScopeConfiguration = storeScope;
+            if (storeScope > 0)
+            {
+                model.Picture1Id_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Picture1Id, storeScope);
+                model.Text1_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Text1, storeScope);
+                model.Link1_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Link1, storeScope);
+                model.Picture2Id_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Picture2Id, storeScope);
+                model.Text2_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Text2, storeScope);
+                model.Link2_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Link2, storeScope);
+                model.Picture3Id_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Picture3Id, storeScope);
+                model.Text3_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Text3, storeScope);
+                model.Link3_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Link3, storeScope);
+                model.Picture4Id_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Picture4Id, storeScope);
+                model.Text4_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Text4, storeScope);
+                model.Link4_OverrideForStore = _settingService.SettingExists(bxSliderSettings, x => x.Link4, storeScope);
+            }
+
+            return View("Nop.Plugin.Widgets.BxSlider.Views.WidgetsNivoSlider.Configure", model);
+        }
+
+        [HttpPost]
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var bxSliderSettings = _settingService.LoadSetting<BxSliderSettings>(storeScope);
+            bxSliderSettings.Picture1Id = model.Picture1Id;
+            bxSliderSettings.Text1 = model.Text1;
+            bxSliderSettings.Link1 = model.Link1;
+            bxSliderSettings.Picture2Id = model.Picture2Id;
+            bxSliderSettings.Text2 = model.Text2;
+            bxSliderSettings.Link2 = model.Link2;
+            bxSliderSettings.Picture3Id = model.Picture3Id;
+            bxSliderSettings.Text3 = model.Text3;
+            bxSliderSettings.Link3 = model.Link3;
+            bxSliderSettings.Picture4Id = model.Picture4Id;
+            bxSliderSettings.Text4 = model.Text4;
+            bxSliderSettings.Link4 = model.Link4;
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared 
+             * and loaded from database after each update */
+            if (model.Picture1Id_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Picture1Id, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Picture1Id, storeScope);
+            
+            if (model.Text1_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Text1, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Text1, storeScope);
+            
+            if (model.Link1_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Link1, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Link1, storeScope);
+            
+            if (model.Picture2Id_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Picture2Id, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Picture2Id, storeScope);
+            
+            if (model.Text2_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Text2, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Text2, storeScope);
+            
+            if (model.Link2_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Link2, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Link2, storeScope);
+            
+            if (model.Picture3Id_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Picture3Id, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Picture3Id, storeScope);
+            
+            if (model.Text3_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Text3, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Text3, storeScope);
+            
+            if (model.Link3_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Link3, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Link3, storeScope);
+            
+            if (model.Picture4Id_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Picture4Id, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Picture4Id, storeScope);
+            
+            if (model.Text4_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Text4, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Text4, storeScope);
+
+            if (model.Link4_OverrideForStore || storeScope == 0)
+                _settingService.SaveSetting(bxSliderSettings, x => x.Link4, storeScope, false);
+            else if (storeScope > 0)
+                _settingService.DeleteSetting(bxSliderSettings, x => x.Link4, storeScope);
+            
+            //now clear settings cache
+            _settingService.ClearCache();
+            
+            return Configure();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PublicInfo(string widgetZone)
+        {
+            var nivoSliderSettings = _settingService.LoadSetting<BxSliderSettings>(_storeContext.CurrentStore.Id);
+
+            var model = new PublicInfoModel();
+            model.Picture1Url = _pictureService.GetPictureUrl(nivoSliderSettings.Picture1Id, showDefaultPicture: false);
+            model.Text1 = nivoSliderSettings.Text1;
+            model.Link1 = nivoSliderSettings.Link1;
+
+            model.Picture2Url = _pictureService.GetPictureUrl(nivoSliderSettings.Picture2Id, showDefaultPicture: false);
+            model.Text2 = nivoSliderSettings.Text2;
+            model.Link2 = nivoSliderSettings.Link2;
+
+            model.Picture3Url = _pictureService.GetPictureUrl(nivoSliderSettings.Picture3Id, showDefaultPicture: false);
+            model.Text3 = nivoSliderSettings.Text3;
+            model.Link3 = nivoSliderSettings.Link3;
+
+            model.Picture4Url = _pictureService.GetPictureUrl(nivoSliderSettings.Picture4Id, showDefaultPicture: false);
+            model.Text4 = nivoSliderSettings.Text4;
+            model.Link4 = nivoSliderSettings.Link4;
+
+            if (string.IsNullOrEmpty(model.Picture1Url) && string.IsNullOrEmpty(model.Picture2Url) &&
+                string.IsNullOrEmpty(model.Picture3Url) && string.IsNullOrEmpty(model.Picture4Url))
+                //no pictures uploaded
+                return Content("");
+            
+
+            return View("Nop.Plugin.Widgets.NivoSlider.Views.WidgetsNivoSlider.PublicInfo", model);
+        }
+    }
+}
